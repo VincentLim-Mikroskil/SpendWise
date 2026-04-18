2fc3992c9f220a497c5de9075961618369190a6c
Kelompok <kelompok@email.com>
2026-04-18 22:22:29 +0700

    ulangi fitur tadi

--- script.js
+++ script.js
@@ -9,17 +9,4 @@ function addExpense() {
 
   let li = document.createElement("li");
   li.innerText = expense + " ";
-
-  // tombol hapus
-  let btn = document.createElement("button");
-  btn.innerText = "Hapus";
-
-  btn.onclick = function () {
-    li.remove(); // ini yang menghapus item
-  };
-
-  li.appendChild(btn);
-  document.getElementById("list").appendChild(li);
-
-  input.value = "";
 }
