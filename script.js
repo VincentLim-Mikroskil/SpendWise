0e14d8747b948651ad99489d5424c46433076f97
Kelompok <kelompok@email.com>
2026-04-19 11:27:00 +0700

    update fitur tambah dan hapus expense

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function addTask() {
-  let task = document.getElementById("task").value;
+function addExpense() {
+  let input = document.getElementById("expense");
+  let expense = input.value.trim();
+
+  if (expense === "") {
+    alert("Tidak boleh kosong!");
+    return;
+  }
+
   let li = document.createElement("li");
-  li.innerText = task;
+  li.innerText = expense + " ";
+
+  // tombol hapus
+  let btn = document.createElement("button");
+  btn.innerText = "Hapus";
+
+  btn.onclick = function () {
+    li.remove(); // ini yang menghapus item
+  };
+
+  li.appendChild(btn);
   document.getElementById("list").appendChild(li);
+
+  input.value = "";
 }
